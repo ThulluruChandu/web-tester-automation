742c37c4ff851f291d2412eaa083f823bc135957
ThulluruChandu <thulluruchandu999@gmail.com>
2026-02-23 14:43:34 +0530

    Create tests/wikipedia-india-capital.spec.ts

---
+++ tests/wikipedia-india-capital.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Wikipedia - India article', () => {
+  test('should list capital as New Delhi', async ({ page }) => {
+    // TODO: implement
+  });
+});
