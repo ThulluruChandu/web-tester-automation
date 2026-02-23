66dfde4c80057d629dc8b6699ca5c816f22243d4
ThulluruChandu <thulluruchandu999@gmail.com>
2026-02-23 16:44:05 +0530

    Create tests/amazon-title.spec.ts

---
+++ tests/amazon-title.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Amazon - page title', () => {
+  test('navigate to Amazon and capture the title', async ({ page }) => {
+    // TODO: implement
+  });
+});
