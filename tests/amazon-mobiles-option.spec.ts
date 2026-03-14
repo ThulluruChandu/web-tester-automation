67644f950ef2c423dc5784ef75e21189eebe09ba
ThulluruChandu <thulluruchandu999@gmail.com>
2026-03-14 20:03:40 +0530

    Create tests/amazon-mobiles-option.spec.ts

---
+++ tests/amazon-mobiles-option.spec.ts
@@ -0,0 +1,7 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('Amazon.com navigation', () => {
+  test('Check whether "Mobiles" option is present', async ({ page }) => {
+    // Test steps will be added.
+  });
+});
